36dc71344e6bd4d61d3c36afe15b4261220e79df
Luis <luisbarcapons@gmail.com>
2020-10-26 22:22:51 +0100

    m

--- Aventura1/my_lib.c
+++ Aventura1/my_lib.c
@@ -8,7 +8,7 @@ size_t my_strlen(const char *str) {
     size_t len = 0;
 
     for (len = 0; len < str[len]; len++)
-    
+
     return len;
 }
 
@@ -26,13 +26,15 @@ int my_strcmp(const char *str1, const char *str2) {
     int i = 0;
     int cmp = 0;
 
-    while(str1[i] && (str1[i] != str2[i])) {
-        if(str1[i] != str2[i]) {
-            cmp = str1[i] - str2[i];
+    while (str1[i] && (str1[i] != str2[i])) {
+        if (str1[i] != str2[i]) {
+            cmp = *(const unsigned char *)str1 - *(const unsigned char *)str2;
+            //cmp = str1[i] - str2[i];
         }
+
         i++;
     }
-    
+
     return cmp;
 }
 
@@ -43,14 +45,12 @@ Devuelve el puntero dest. No devuelve error.
 */
 char *my_strcpy(char *dest, const char *src) {
     int c = 0;
-    char *org = dest;
-
     for (c; src[c]; c++) {
         dest[c] = src[c];
     }
-    dest[c] = '\0';
 
-    return org;    
+    dest[c] = '\0';
+    return dest;
 }
 
 /*
@@ -58,31 +58,36 @@ La función strncpy() copia n caracteres de la cadena apuntada por src (con el c
 en la memoria apuntada por dest. Devuelve el puntero dest. No devuelve error.
 En caso de que strlen(src) < n, el resto de la cadena apuntada por dest ha de ser rellenado con 0s. 
 */
-char *my_strncpy(char *dest, const char *src, size_t n) {
+char *my_strncpy(char *dest, const char *src, size_t n)
+{
+    char* paux = dest;
     int len = my_strlen(src);
-    if(n <= len){ //Copiamos n caracteres sin '\0'
-        while(n){
-            *dest = *src;
-            dest++;
+    if (n <= len) { //Copiamos n caracteres sin '\0'
+        while (n) {
+            *paux = *src;
+            paux++;
             src++;
             n--;
         }
-    }else{ //Copiamos n carácteres y rellenamos con '\0'
-        int k = n - (len + 1);
-        while(len){
-            *dest = *src;
-            dest++;
+    }else { //Copiamos todos los carácteres y rellenamos con '\0'
+        int resto = n - (len + 1);
+
+        while (len) {
+            *paux = *src;
+            paux++;
             src++;
             len--;
         }
+
         //Una vez copiados los carácteres añadimos '\0'
-        *dest = '\0';
-        while(k){
-            dest++;
-            k--;
-            *dest = '\0';
+        *paux = '\0';
+        while (resto) {
+            paux++;
+            resto--;
+            *paux = '\0';
         }
     }
+
     return dest;
 }
 
@@ -93,16 +98,24 @@ El primer carácter de src sobreescribe el carácter nulo de dest.
 Devuelve el puntero dest. No devuelve error.
 */
 char *my_strcat(char *dest, const char *src) {
-    int c = 0;
-    int size_dest = sizeof dest / sizeof dest[0];
-    int size_src = sizeof src / sizeof src[0];
-    char new_dest [size_dest + size_src];
+    int size_dest = sizeof(dest);
+    int size_src = sizeof(src);
+    char new_dest[size_dest + size_src];
+    int i = 0;
+    int j = 0;
 
-    my_strcpy(new_dest[0], dest);
-    for (c; c < size_src; c++) {
-        new_dest[size_dest + c] = src[c];
+    // Concatenamos la primera secuencia de caracteres al nuevo array
+    while (i < size_dest) {
+        new_dest[i] = dest[i];
+        i++;
     }
-    new_dest[c] = '\0';
-    
+
+    // Concatenamos la segunda secuencia de caracteres también al nuevo array
+    for (i; j < size_src; i++) {
+        new_dest[i] = src[j];
+        j++;
+    }
+    new_dest[i] = '\0';
+
     return new_dest;
 }
