de1b166cd2f549f70b0b871551f77e8e8605e262
Luis <luisbarcapons@gmail.com>
2020-10-31 10:41:44 +0100

    mylib con reto2

--- Aventura1/my_lib.c
+++ Aventura1/my_lib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "my_lib.h"
 
 /*
 Esta función calcula el nº de bytes de la cadena apuntada por str, sin incluir el carácter nulo de terminación ‘\0’ .
@@ -116,4 +117,130 @@ char *my_strcat(char *dest, const char *src) {
         *dest = '\0'; 
     
     return new_dest;
+}
+
+/*
+Reserva espacio para una variable de tipo struct my_stack, que contendrá el puntero al nodo 
+superior de la pila y el tamaño de los datos, e inicializa esos valores con NULL, como valor 
+del puntero al nodo superior de la pila, y con el tamaño de datos que nos pasan como parámetro.
+
+Devuelve un puntero a la pila inicializada.
+*/
+struct my_stack *my_stack_init(int size) {
+    struct my_stack *stack = malloc(sizeof(struct my_stack));
+    stack->first = NULL;
+    stack->size = size;
+
+    return stack;
+}
+
+/*
+Inserta un nuevo nodo en los elementos de la pila (hay que reservar espacio de memoria para él).
+El puntero a los datos de ese nodo nos lo pasan como parámetro.
+*/
+int my_stack_push(struct my_stack *stack, void *data) {
+    int r = -1;
+
+    if (stack) {
+        if (stack->size > 0) {   
+            //Creamos un nuevo nodo con los datos de los parámetros
+            //Y "actualizamos" el nuevo "top" de la pila, que sería nuestro first.
+            struct my_stack_node *new_node = malloc(sizeof(new_node));
+            new_node->data = data;
+            new_node->next = stack->first;
+
+            stack->first = new_node;
+            r = 0;
+        }
+    }
+
+    return r;
+}
+
+/*
+Elimina el nodo superior de los elementos de la pila (y libera la memoria que ocupaba ese nodo!).
+Devuelve el puntero a los datos del elemento eliminado. 
+
+Si no existe nodo superior (pila vacía), retorna NULL.
+*/
+
+void *my_stack_pop(struct my_stack *stack) {
+    if (stack->first) {
+        struct my_stack_node *temp_node = malloc(sizeof(temp_node)); //PREGUNNTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        temp_node->next = stack->first;
+        //stack->first = temp_node->next;
+        void *datos = temp_node->data;
+        
+        free(temp_node);
+
+        return datos;
+    }else {
+        return NULL;
+    }
+}
+
+/*
+void *my_stack_pop(struct my_stack *stack)
+{
+    if (stack->first)
+    {
+        struct my_stack_node *temp_node = malloc(sizeof(temp_node)); //PREGUNNTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        struct my_stack_node *delte_node = malloc(sizeof(delte_node));
+
+        delte_node = stack->first;
+        temp_node = stack->first;
+
+        stack->first = temp_node->next;
+
+        //free(delte_node);
+        return temp_node;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+*/
+
+/*
+Recorre la pila y retorna el número de nodos totales que hay en los elementos de la pila.
+*/
+int my_stack_len(struct my_stack *stack) {
+    int contador = 0;
+    struct my_stack_node *temp_node = malloc(sizeof(temp_node));
+    temp_node = stack->first;
+
+    //Recorrido hasta el final de la pilay augmentamos el contador
+    while (temp_node) {
+        temp_node = temp_node->next;
+        contador++;
+    }
+    free(temp_node);    //PREGUNNTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    return contador;
+}
+
+/*
+Recorre la pila liberando la memoria que habíamos reservado para cada uno de los datos 
+(data) y la de cada nodo. Finalmente libera también la memoria que ocupa la pila. Es decir, toda la memoria que se reservó con malloc() en algún momento, se libera con la función free(). 
+
+Devuelve el número de bytes liberados.  
+*/
+int my_stack_purge (struct my_stack *stack) {
+    int contador = 0;
+    struct my_stack_node *temp_node = malloc(sizeof(temp_node));
+    temp_node = stack->first;
+
+    //Recorrido hasta el final de la pilay augmentamos el contador
+    while (temp_node) {
+        temp_node = temp_node->next;
+        contador++;
+    }
+
+    contador *= sizeof(temp_node);
+    contador += sizeof(stack);
+
+    free(temp_node);    //PREGUNNTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    return contador;
 }
