344abb0523a3832d52c6894e66831d33b82f461a
Luis <luisbarcapons@gmail.com>
2020-11-11 22:38:13 +0100

    perfeccion codigo nivel 1

--- Aventura2/nivel1.c
+++ Aventura2/nivel1.c
@@ -24,10 +24,8 @@ int internal_jobs(char **args);
 */
 int main()
 {
-    while (1)
-    {
-        if (read_line(line) && strlen(line) > 0)
-        {
+    while (1) {
+        if (read_line(line) && strlen(line) > 0) {
             execute_line(line);
         }
     }
@@ -38,22 +36,19 @@ int main()
 /*
 *   Método para imprimir el PROMPT
 */
-void imprimir_prompt()
-{
-    
+void imprimir_prompt() {
     char *prompt = malloc(sizeof(char) * COMMAND_LINE_SIZE);
 
     //Get USERNAME
     char *user = getenv("USER");
     getcwd(prompt, COMMAND_LINE_SIZE);
     // Gets the current work directory.
-    //getcwd(aux, COMMAND_LINE_SIZE);
+    // getcwd(aux, COMMAND_LINE_SIZE);
     // Prints the prompt and the separator.
     printf("%s: %s %c:", user, prompt, PROMPT);
 
     // frees the memory for prompt.
     free(prompt);
-    
 
     //PS1='$(tput bold; tput setaf 1)\u$(tput sgr0):$(tput bold; tput setaf 6)\w$(tput sgr0)$ '
     //printf("%c ", PROMPT);
@@ -63,14 +58,12 @@ void imprimir_prompt()
 /*
 Leer una linea de la consola
 */
-char *read_line(char *line)
-{
+char *read_line(char *line) {
     imprimir_prompt();
 
     // Reads input introduced in stdin by the user.
     // Control de errores
-    if (fgets(line, COMMAND_LINE_SIZE, stdin) == NULL)
-    {
+    if (fgets(line, COMMAND_LINE_SIZE, stdin) == NULL) {
         perror("Error al leer la línea");
     }
 
@@ -80,23 +73,18 @@ char *read_line(char *line)
 /*
 *
 */
-int execute_line(char *line)
-{
-
+int execute_line(char *line) {
     //Reservamos memoria para los tokens
-
     char **args = malloc(sizeof(char *) * ARGS_SIZE);
-    if (args == NULL)
-    {
+    
+    if (args == NULL) {
         fprintf(stderr, "Memoria dinamica llena.");
     }
 
-    if (args)
-    {
+    if (args) {
         //Parseamos
         parse_args(args, line);
-        if (args[0])
-        {
+        if (args[0]) {
             check_internal(args);
         }
     }
@@ -105,8 +93,7 @@ int execute_line(char *line)
 }
 
 //{line hacer esto #para esto} args[x][0]
-int parse_args(char **args, char *line)
-{
+int parse_args(char **args, char *line) {
     int nToken = 0;
     const char s[5] = " \t\r\n";
     char *token;
@@ -114,16 +101,12 @@ int parse_args(char **args, char *line)
     token = strtok(line, s);
     args[nToken] = token;
 
-    while (token != NULL)
-    {
+    while (token != NULL) {
         printf("[parse_args() → token %d: %s]\n", nToken, token);
         //Descartamos comentarios
-        if (*(token) != '#')
-        {
+        if (*(token) != '#') {
             args[nToken] = token;
-        }
-        else
-        {
+        } else {
             //Añadimos NULL
             token = NULL;
             args[nToken] = token;
@@ -135,11 +118,11 @@ int parse_args(char **args, char *line)
         token = strtok(NULL, s);
         nToken++;
     }
+
     return nToken;
 }
 
-int check_internal(char **args)
-{
+int check_internal(char **args) {
     int comandoInterno = 0;
 
     const char cd[] = "cd";
@@ -150,38 +133,25 @@ int check_internal(char **args)
     const char bg[] = "bg";
     const char exit[] = "exit";
 
-    if (!strcmp(args[0], cd))
-    {
+    if (!strcmp(args[0], cd)) {
         internal_cd(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], export))
-    {
+    } else if (!strcmp(args[0], export)) {
         internal_export(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], source))
-    {
+    } else if (!strcmp(args[0], source)) {
         internal_source(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], jobs))
-    {
+    } else if (!strcmp(args[0], jobs)) {
         internal_jobs(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], fg))
-    {
+    } else if (!strcmp(args[0], fg)) {
         internal_fg(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], bg))
-    {
+    } else if (!strcmp(args[0], bg)) {
         internal_bg(args);
         comandoInterno = 1;
-    }
-    else if (!strcmp(args[0], exit))
-    {
+    } else if (!strcmp(args[0], exit)) {
         internal_exit(args);
         comandoInterno = 1;
     }
@@ -189,56 +159,49 @@ int check_internal(char **args)
     return comandoInterno;
 }
 
-int internal_cd(char **args)
-{
+int internal_cd(char **args) {
 #if DEBUG
     printf("[internal_cd()→ Esta función cambiará de directorio]\n");
 #endif
     return 1;
 }
 
-int internal_export(char **args)
-{
+int internal_export(char **args) {
 #if DEBUG
     printf("[internal_export()→Esta función asignará valores a variables de entorno]\n");
 #endif
     return 1;
 }
 
-int internal_source(char **args)
-{
+int internal_source(char **args) {
 #if DEBUG
     printf("[internal_source()→Esta función ejecutará un fichero de líneas de comandos]\n");
 #endif
     return 1;
 }
 
-int internal_jobs(char **args)
-{
+int internal_jobs(char **args) {
 #if DEBUG
     printf("[internal_jobs()→Esta función mostrará el PID de los procesos que no estén en foreground]\n");
 #endif
     return 1;
 }
 
-int internal_fg(char **args)
-{
+int internal_fg(char **args) {
 #if DEBUG
     printf("[internal_fg()→Esta función parsará/activará a primer plano procesos]\n");
 #endif
     return 1;
 }
 
-int internal_bg(char **args)
-{
+int internal_bg(char **args) {
 #if DEBUG
     printf("[internal_bg()→Esta función parsará/activará a segundo plano procesos]\n");
 #endif
     return 1;
 }
 
-int internal_exit(char **args)
-{
+int internal_exit(char **args) {
 #if DEBUG
     printf("[internal_exit()→Esta función sale del mini shell]\n");
 #endif
